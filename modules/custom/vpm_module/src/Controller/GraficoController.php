b13f881d9d356caa5f4b1a21c4d9e68057415b6d
hgacitua <44929346+hgacitua@users.noreply.github.com>
2022-05-09 18:20:14 -0400

    grafico commit

--- modules/custom/vpm_module/src/Controller/GraficoController.php
+++ modules/custom/vpm_module/src/Controller/GraficoController.php
@@ -21,6 +21,12 @@ class GraficoController extends ControllerBase
         $sql = "IFNULL(terminoTaxSoporte.name, 'Desconocido') as EjeY";
       } elseif ($valorCorY == 5) { // SI SELECCIONÓ AUTOR
         $sql = "IFNULL(terminoTaxAutoria.name, 'Desconocido') as EjeY";
+      } elseif ($valorCorY == 8) { // SI SELECCIONÓ GÉNERO
+        $sql = "IFNULL(fdfg.field_genero_value, 'Desconocido') as EjeY";
+      }elseif ($valorCorY == 9) { // SI SELECCIONÓ ACTIVIDAD O PROFESIÓN
+        $sql = "IFNULL(terminoTaxEActiProf.name, 'Desconocido') as EjeY";
+      }elseif ($valorCorY == 10) { // SI SELECCIONÓ ETNIA O RAZA
+        $sql = "IFNULL(terminoTaxEtnia.name, 'Desconocido') as EjeY";
       }
 
       $query = "SELECT
@@ -39,6 +45,20 @@ class GraficoController extends ControllerBase
       LEFT JOIN taxonomy_term_data terminoTaxTecnica ON terminoTaxTecnica.tid = tecnicaObra.field_tecnica_tid
       LEFT JOIN field_data_field_soporte soporte ON soporte.entity_id = iden.field_identificacion_value
       LEFT JOIN taxonomy_term_data terminoTaxSoporte ON terminoTaxSoporte.tid = soporte.field_soporte_tid
+
+      LEFT JOIN field_data_field_iconografia_retrato fdfir on fdfir.entity_id = node.nid 
+      LEFT JOIN field_data_field_persona fdfp on fdfir.field_iconografia_retrato_value = fdfp.entity_id 
+      LEFT JOIN field_data_field_genero fdfg on fdfp.field_persona_value = fdfg.entity_id 
+
+      LEFT JOIN field_data_field_persona actividad on fdfir.field_iconografia_retrato_value = actividad.entity_id 
+      LEFT JOIN field_data_field_actividad_o_profesion fdfaop on actividad.field_persona_value = fdfaop.entity_id 
+      LEFT JOIN taxonomy_term_data terminoTaxEActiProf ON terminoTaxEActiProf.tid = fdfaop.field_actividad_o_profesion_tid 
+
+      LEFT JOIN field_data_field_persona etnia on fdfir.field_iconografia_retrato_value = etnia.entity_id
+      LEFT JOIN field_data_field_etnico_racial fdfer on etnia.field_persona_value = fdfer.entity_id  
+      LEFT JOIN taxonomy_term_data terminoTaxEtnia ON terminoTaxEtnia.tid = fdfer.field_etnico_racial_tid 
+
+
       WHERE node.type = 'obra' AND node.status=1";
       if ($valorCorY == 1) { // SI SELECCIONÓ OBRAS
         $query .= " GROUP BY IFNULL(DATE_FORMAT(fecEjecucion.field_fecha_ejecucion_timestamp, '%Y'), 'Desconocido')";
@@ -108,6 +128,15 @@ class GraficoController extends ControllerBase
       }elseif ($valorCorY == 6) { // SI SELECCIONÓ AÑO
         $sql = "IFNULL(DATE_FORMAT(fecEjecucion.field_fecha_ejecucion_timestamp, '%Y'), 'Desconocido')";
         $nombre = "Año";
+      } elseif ($valorCorY == 8) { // SI SELECCIONÓ GÉNERO
+        $sql = "IFNULL(fdfg.field_genero_value, 'Desconocido') as EjeY";
+        $nombre = "Género";
+      }elseif ($valorCorY == 9) { // SI SELECCIONÓ ACTIVIDAD O PROFESIÓN
+        $sql = "IFNULL(terminoTaxEActiProf.name, 'Desconocido') as EjeY";
+        $nombre = "Actividad o Profesión";
+      }elseif ($valorCorY == 10) { // SI SELECCIONÓ ETNIA O RAZA
+        $sql = "IFNULL(terminoTaxEtnia.name, 'Desconocido') as EjeY";
+        $nombre = "Etnia o Raza";
       }
       $query = "SELECT COUNT(nid)  as Obra,
       ".$sql." as EjeY     
@@ -180,134 +209,3 @@ class GraficoController extends ControllerBase
     ];
   }
 }
-
-
-/*elseif($valorCorX == 2){ 
-         $mysqli = new mysqli('127.0.0.1', 'root', '', 'quinsac');
-         $query ="SELECT
-         DATE_FORMAT(fecEjecucion.field_fecha_ejecucion_timestamp, '%Y') as fecEjec,
-         IFNULL(terminoTaxTecnica.name, 'Desconocido') as Tecnica
-         FROM node
-         LEFT JOIN field_data_field_identificacion iden ON iden.entity_id = node.nid
-         LEFT JOIN field_data_field_fecha_ejecucion fecEjecucion ON fecEjecucion.entity_id = iden.field_identificacion_value
-         LEFT JOIN field_data_field_autoria_principal autoria ON autoria.entity_id = iden.field_identificacion_value
-         LEFT JOIN taxonomy_term_data terminoTaxAutoria ON terminoTaxAutoria.tid = autoria.field_autoria_principal_tid
-         LEFT JOIN field_data_field_imagen ON field_data_field_imagen.entity_id = iden.field_identificacion_value
-         LEFT JOIN file_managed ON file_managed.fid = field_data_field_imagen.field_imagen_fid
-         LEFT JOIN field_data_field_tematica_de_la_obra tematicaObra ON tematicaObra.entity_id = iden.field_identificacion_value
-         LEFT JOIN taxonomy_term_data terminoTaxTematica ON terminoTaxTematica.tid = tematicaObra.field_tematica_de_la_obra_tid 
-         LEFT JOIN field_data_field_tecnica tecnicaObra ON tecnicaObra.entity_id = iden.field_identificacion_value
-         LEFT JOIN taxonomy_term_data terminoTaxTecnica ON terminoTaxTecnica.tid = tecnicaObra.field_tecnica_tid
-         WHERE node.type = 'obra' AND node.status=1 AND DATE_FORMAT(fecEjecucion.field_fecha_ejecucion_timestamp, '%Y') IS NOT NUll";
-         
-         $resultado = $mysqli->query($query);
-                  
-         $xValues = "";
-         $yValues = "";
-         $stringColor ="";
-         $cantObras=1;
-         $prov='';
-         
-         while ($fila = mysqli_fetch_array($resultado)) { 
-          if($prov!="'Técnica: ".substr($fila['Tecnica'],0,100)." - Año: ".$fila['fecEjec']."',"){
-            $yValues.=$cantObras.",";      
-            $cantObras=1;
-                    
-            $xValues.="'Técnica: ".substr($fila['Tecnica'],0,100)." - Año: ".$fila['fecEjec']."',"; 
-            $prov="'Técnica: ".substr($fila['Tecnica'],0,100)." - Año: ".$fila['fecEjec']."',"; 
-            $stringColor .=  "'".$this->colorRGB()."',"; 
-          }else{
-            $cantObras++;
-          }      
-        }
-        mysqli_close($mysqli);
-        $grafico['yValues'] = $yValues;
-        $grafico['xValues'] = $xValues;
-        $grafico['stringColor'] = $stringColor;
-        return $grafico;
-
-    }elseif($valorCorX == 3){ 
-      $mysqli = new mysqli('127.0.0.1', 'root', '', 'quinsac');
-
-      $query ="SELECT count(IFNULL(title, 'Desconocido'))  as Obra,
-      DATE_FORMAT(fecEjecucion.field_fecha_ejecucion_timestamp, '%Y') as fecEjec
-      FROM node
-      LEFT JOIN field_data_field_identificacion iden ON iden.entity_id = node.nid
-      LEFT JOIN field_data_field_fecha_ejecucion fecEjecucion ON fecEjecucion.entity_id = iden.field_identificacion_value
-      LEFT JOIN field_data_field_autoria_principal autoria ON autoria.entity_id = iden.field_identificacion_value
-      LEFT JOIN taxonomy_term_data terminoTaxAutoria ON terminoTaxAutoria.tid = autoria.field_autoria_principal_tid
-      LEFT JOIN field_data_field_imagen ON field_data_field_imagen.entity_id = iden.field_identificacion_value
-      LEFT JOIN file_managed ON file_managed.fid = field_data_field_imagen.field_imagen_fid
-      LEFT JOIN field_data_field_tematica_de_la_obra tematicaObra ON tematicaObra.entity_id = iden.field_identificacion_value
-      LEFT JOIN taxonomy_term_data terminoTaxTematica ON terminoTaxTematica.tid = tematicaObra.field_tematica_de_la_obra_tid 
-      LEFT JOIN field_data_field_tecnica tecnicaObra ON tecnicaObra.entity_id = iden.field_identificacion_value
-      LEFT JOIN taxonomy_term_data terminoTaxTecnica ON terminoTaxTecnica.tid = tecnicaObra.field_tecnica_tid
-      WHERE node.type = 'obra' AND node.status=1
-      AND DATE_FORMAT(fecEjecucion.field_fecha_ejecucion_timestamp, '%Y') IS NOT null
-      group by DATE_FORMAT(fecEjecucion.field_fecha_ejecucion_timestamp, '%Y')";
-      
-      $resultado = $mysqli->query($query);
-               
-      $xValues = "";
-      $yValues = "";
-      $stringColor ="";
-      $cantObras=1;
-      $prov='';
-      
-      while ($fila = mysqli_fetch_array($resultado)) { 
-       $yValues.=$fila['Obra'].",";
-       $xValues.="'Cantidad de Obras: ".substr($fila['Obra'],0,100)." - Año: ".$fila['fecEjec']."',"; 
-       $stringColor .=  "'".$this->colorRGB()."',"; 
-     }
-     mysqli_close($mysqli);
-     $grafico['yValues'] = $yValues;
-     $grafico['xValues'] = $xValues;
-     $grafico['stringColor'] = $stringColor;
-     return $grafico;
-
-    }elseif($valorCorX == 4){ 
-      $mysqli = new mysqli('127.0.0.1', 'root', '', 'quinsac');
-      $query ="SELECT 
-      IFNULL(terminoTaxAutoria.name, 'Desconocido') as autor,
-      DATE_FORMAT(fecEjecucion.field_fecha_ejecucion_timestamp, '%Y') as fecEjec
-      FROM node
-      LEFT JOIN field_data_field_identificacion iden ON iden.entity_id = node.nid
-      LEFT JOIN field_data_field_fecha_ejecucion fecEjecucion ON fecEjecucion.entity_id = iden.field_identificacion_value
-      LEFT JOIN field_data_field_autoria_principal autoria ON autoria.entity_id = iden.field_identificacion_value
-      LEFT JOIN taxonomy_term_data terminoTaxAutoria ON terminoTaxAutoria.tid = autoria.field_autoria_principal_tid
-      LEFT JOIN field_data_field_imagen ON field_data_field_imagen.entity_id = iden.field_identificacion_value
-      LEFT JOIN file_managed ON file_managed.fid = field_data_field_imagen.field_imagen_fid
-      LEFT JOIN field_data_field_tematica_de_la_obra tematicaObra ON tematicaObra.entity_id = iden.field_identificacion_value
-      LEFT JOIN taxonomy_term_data terminoTaxTematica ON terminoTaxTematica.tid = tematicaObra.field_tematica_de_la_obra_tid 
-      LEFT JOIN field_data_field_tecnica tecnicaObra ON tecnicaObra.entity_id = iden.field_identificacion_value
-      LEFT JOIN taxonomy_term_data terminoTaxTecnica ON terminoTaxTecnica.tid = tecnicaObra.field_tecnica_tid
-      WHERE node.type = 'obra' AND node.status=1
-      AND DATE_FORMAT(fecEjecucion.field_fecha_ejecucion_timestamp, '%Y') IS NOT NUll";
-      
-      $resultado = $mysqli->query($query);
-               
-      $xValues = "";
-      $yValues = "";
-      $stringColor ="";
-      $cantObras=1;
-      $prov='';
-      
-      while ($fila = mysqli_fetch_array($resultado)) { 
-       if($prov!="'Artista: ".substr($fila['autor'],0,50)." - Año: ".$fila['fecEjec']."',"){
-         $yValues.=$cantObras.",";      
-         $cantObras=1;
-                 
-         $xValues.="'Artista: ".substr($fila['autor'],0,50)." - Año: ".$fila['fecEjec']."',"; 
-         $prov="'Artista: ".substr($fila['autor'],0,50)." - Año: ".$fila['fecEjec']."',"; 
-         $stringColor .=  "'".$this->colorRGB()."',"; 
-       }else{
-         $cantObras++;
-       }      
-     }
-     mysqli_close($mysqli);
-     $grafico['yValues'] = $yValues;
-     $grafico['xValues'] = $xValues;
-     $grafico['stringColor'] = $stringColor;
-     return $grafico;
-    }
-  }*/
